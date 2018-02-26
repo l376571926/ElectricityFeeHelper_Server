fe870adf9ffa7d7e08b9ddb6227ac2bbc9f375d1
l376571926 <15897461476@139.com>
2018-02-26 20:35:41 +0800

    init

---
+++ src/main/java/group/tonight/electricityfeehelper_server/ElectricityFeeHelperServerApplication.java
@@ -0,0 +1,12 @@
+package group.tonight.electricityfeehelper_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityFeeHelperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityFeeHelperServerApplication.class, args);
+	}
+}
